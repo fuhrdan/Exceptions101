a8f65a0afbd9ac3d46c572a687556507643c52e0
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-05-09 11:03:52 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿ArgumentException invalidArgumentException = new ArgumentException();
+
+ArgumentException invalidArgumentException = new ArgumentException("ArgumentException: The 'GraphData' method received data outside the expected range.");
+throw invalidArgumentException;
+throw new FormatException("FormatException: Calculations in process XYZ have been cancelled due to invalid data format.");
